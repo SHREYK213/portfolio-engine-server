92c8eaa6ddff9b66180101bc74067bc7771a55a8
shreyas <shreyaspoojary240@gmail.com>
2026-05-08 00:20:57 +0530

    feat: configure JWT module with async registration and environment variables

--- src/modules/auth/auth.module.ts
+++ src/modules/auth/auth.module.ts
@@ -1,7 +1,21 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { JwtModule } from '@nestjs/jwt';
 import { AuthService } from './auth.service';
 
 @Module({
-  providers: [AuthService]
+  imports: [
+    JwtModule.registerAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configureService: ConfigService) => ({
+        secret: configureService.getOrThrow('JWT_SECRET'),
+        signOptions: {
+          expiresIn: configureService.getOrThrow('JWT_EXPIRES_IN'),
+        },
+      }),
+    }),
+  ],
+  providers: [AuthService],
 })
 export class AuthModule {}
